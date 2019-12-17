29e998e231765c6ab9995b5a35ba69d16d1f7741
Scott Sum <scottsum@outlook.com>
2019-12-17 10:04:35 -0500

    changing database to mysql

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
@@ -11,17 +12,22 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-app = Flask(__name__)
+
+
+app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scottsum:mysequelpassword@localhost/site_db'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False # no sqlalchemy warnings in console
+bootstrap = Bootstrap(app) # allows use of flask-bootstrap
+db = SQLAlchemy(app) # database
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model): # creating table
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
@@ -41,20 +47,26 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-#user agent gives info about browser
+# user agent gives info about browser
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
 ########################################################################################################################################################
+# gets information from price tracking form, parses the amazon page and finds the price
+# if the price is under the budget, an email is sent to the user
 def check_price(): 
     URL = request.form.get('product')   
     budget = request.form.get('budget')
     mail = request.form.get('email')
+
+    # essentially downloads the page
     page = requests.get(URL, headers=headers)
-    
+    # uses bs4 and lxml parser
     soup = BeautifulSoup(page.content, 'lxml')
+    
     price = soup.find(id="price_inside_buybox").text.strip()
     converted_price = float(price[5:8])
-
+    
+    # only send the email if price is under budget
     if (converted_price < float(budget)):
         send_mail(mail, URL)
         return 0
@@ -62,7 +74,7 @@ def check_price():
         return 1
         
 
-
+# sends an email to address with the link to the product
 def send_mail(address, link):   
      
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -72,8 +84,10 @@ def send_mail(address, link):
     server.starttls()
     server.ehlo()
 
+    # google two factor authentication
     server.login('scotttnsum@gmail.com', 'cpmhhoretmxmajmk')
 
+    # configuring the contents of the email
     subject = 'Price fell down! The price is now right!'
     body = f'Check the amazon link! {link}'
 
@@ -87,17 +101,20 @@ def send_mail(address, link):
 
     print('Email sent')
     
+    # remember to leave the server when finished sending email
     server.quit()
     
 
 ##########################################################################################################################################################################3
 
+# homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
 ##################################################################
 
+# login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -114,7 +131,7 @@ def login():
     return render_template('login.html', form=form)
 
 #########################################################################
-
+# signup page (similar to login route)
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
@@ -131,19 +148,20 @@ def signup():
 
 ########################################################################################################3
 
+# dashboard page or the page with the price tracking form
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.email)
+    return render_template('dashboard.html', name=current_user.email) # gets the email of the user that is currently logged in
 
 
 @app.route('/dashboard', methods=['POST'])
 @login_required
 def submission():
     while(True):        
-        if (check_price() == 0):
-            break
-        time.sleep(86400)    
+        if (check_price() == 0):  # check_price() == 0 means that the price is under the budget and an email was sent
+            break # since the email was sent, we can exit the loop
+        time.sleep(86400)  # keeps checking until the price falls below the budget   
     return redirect(url_for('index'))
 
 ########################################################################################
